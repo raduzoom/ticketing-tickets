b226f2aab4d494abf629f7e7e78f2d568b038c2d
Radu Hulubas <radu.hulubas@softvision.com>
2022-04-24 14:19:43 +0300

    fix: change to common lib

--- src/events/publishers/ticket-created-publisher.ts
+++ src/events/publishers/ticket-created-publisher.ts
@@ -1,4 +1,4 @@
-import { Publisher, Subjects, TicketCreatedEvent } from '@cygnetops/common';
+import { Publisher, Subjects, TicketCreatedEvent } from '@mytuts/common';
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
--- src/events/publishers/ticket-updated-publisher.ts
+++ src/events/publishers/ticket-updated-publisher.ts
@@ -1,4 +1,4 @@
-import { Publisher, Subjects, TicketUpdatedEvent } from '@cygnetops/common';
+import { Publisher, Subjects, TicketUpdatedEvent } from '@mytuts/common';
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
