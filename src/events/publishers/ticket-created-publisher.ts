f076c6f49a0c134203f2d7dd874adf694c1cb3e4
Radu Hulubas <radu.hulubas@softvision.com>
2022-04-28 18:45:19 +0300

    fix: listeners

--- src/events/publishers/ticket-created-publisher.ts
+++ src/events/publishers/ticket-created-publisher.ts
@@ -1,4 +1,4 @@
-import { Publisher, Subjects, TicketCreatedEvent } from '@mytuts/common';
+import { Publisher, Subjects, TicketCreatedEvent } from '@cygnetops/common';
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
--- src/routes/update.ts
+++ src/routes/update.ts
@@ -5,7 +5,7 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
-} from '@mytuts/common';
+} from '@cygnetops/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { natsWrapper } from '../nats-wrapper';
